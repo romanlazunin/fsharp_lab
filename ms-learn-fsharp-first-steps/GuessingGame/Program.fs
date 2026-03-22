780c53bcaa0d809ecefa546e3b1d7f946cf4d365
Roman Lazunin <rlazunin@gmail.com>
2026-03-22 23:06:49 +0100

    scaffold app and add code for GuessingGame and Loops

---
+++ ms-learn-fsharp-first-steps/GuessingGame/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+
+let mutable quit = false
+let no = 11
+while not quit do
+   printf "Guess a number: "
+   let guess = Console.ReadLine() 
+   let guessNo = int guess
+   if guessNo = no then
+      quit <- true
+      printfn "You guessed correctly %i is the secret number" no
+   else
+      printfn "%i is incorrect" guessNo
---
+++ ms-learn-fsharp-first-steps/Loops/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+
+let suit (no:int) : string = 
+    let suitNo:int = no / 13
+    if suitNo = 0 then "Hearts"
+    elif suitNo = 1 then "Spades"
+    elif suitNo = 2 then "Diamonds"
+    else "Clubs" 
+
+let cardDescription (card: int) : string =
+    let cardNo: int = card % 13
+    if cardNo = 1 then "Ace"
+    elif cardNo = 11 then "Jack"
+    elif cardNo = 12 then "Queen"
+    elif cardNo = 0 then "King"
+    else string cardNo
+
+[<EntryPoint>]
+let main argv =
+
+    let cards = [ 1; 10; 2; 34 ]
+
+    for card in cards do
+      printfn "%s of %s" (cardDescription(card)) (suit(card))
+    0 // return an integer exit code
