569f0afc03bf10ef40d8b8a1671be402baaa0c5a
Roman Lazunin <rlazunin@gmail.com>
2026-03-22 22:02:15 +0100

    add different types variables; add console output

--- ms-learn-fsharp-first-steps/MyFSharpApp/Program.fs
+++ ms-learn-fsharp-first-steps/MyFSharpApp/Program.fs
@@ -14,4 +14,31 @@ let main argv =
     let squares = List.map square numbers
     printfn "N^2 = %A" squares
 
+    let name = "Chris"
+
+    let mutable name = "Tobias"
+    name <- "Luis" // this statement is now allowed
+
+    let goodWeatherToday = true
+    let badWeatherToday = false
+
+    let currentYear = 2026
+    let greeting = "Hej"
+    let oneAndAHalf = 1.5
+    let twoAndAHalf:float = 2.5
+    printfn "Greeting: %s" greeting
+
+    Console.WriteLine("Console.WriteLine output")
+
+    let name = "Luis"
+    let company = "Microsoft"
+    printfn $"Name: {name}, Company: {company}"
+
+    let firstNumber = 2000
+    let secondNumber = 26
+    printfn $"The year is: {firstNumber + secondNumber}"
+
+    printfn "Current year: %i" (firstNumber + secondNumber)
+    printfn "The weather is good today: %b" goodWeatherToday
+
     0 // return an integer exit code
