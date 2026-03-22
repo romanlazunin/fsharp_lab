7b043de152d5d213404dea10fac497ae48fbf834
Roman Lazunin <rlazunin@gmail.com>
2026-03-22 22:44:58 +0100

    add conditional code to print card name

---
+++ ms-learn-fsharp-first-steps/Cards/Program.fs
@@ -0,0 +1,9 @@
+﻿let cardNo = 12
+
+let cardDescription = 
+   if cardNo = 1 || cardNo = 14 then "Ace"
+   elif cardNo = 11 then "Jack"
+   elif cardNo = 12 then "Queen"
+   elif cardNo = 13 then "King"
+   else string cardNo
+printfn "%s" cardDescription
