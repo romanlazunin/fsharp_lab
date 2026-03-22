acea5c142f2b7877b3236e2ce68828105e0dd3e1
Roman Lazunin <rlazunin@gmail.com>
2026-03-22 22:27:09 +0100

    add input from console; arithmetic operations

--- ms-learn-fsharp-first-steps/Errors/Program.fs
+++ ms-learn-fsharp-first-steps/Errors/Program.fs
@@ -1,2 +1,24 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    let aNumber = 0
+    printfn "Here's a number %i" aNumber
+
+    Console.Write "Type a value:"
+    let str = Console.ReadLine()
+    printfn "You typed %s" str
+
+    let first = "32"
+    let numberVersion = System.Int32.Parse first 
+    printfn "Number %i" numberVersion // Output: Number 32
+
+    let second = "34"
+    let secondNumberVersion =  int second
+    printfn "Number %i" secondNumberVersion
+
+    let no = 10
+    let isDivisibleByTwo = no % 2 = 0
+    printfn "Divisible by two %b" isDivisibleByTwo
+
+    0 // return an integer exit code
--- ms-learn-fsharp-first-steps/Variables/Program.fs
+++ ms-learn-fsharp-first-steps/Variables/Program.fs
@@ -1,7 +1,15 @@
-﻿[<EntryPoint>]
+﻿open System
+
+[<EntryPoint>]
 let main argv =
     printfn "Welcome to the calculator program"
-    // read input from the console and assign to `sum`
-    let sum = 0
+    
+    printfn "Type the first number"
+    let firstNo = Console.ReadLine()
+    printfn "Type the second number"
+    let secondNo = Console.ReadLine()
+    printfn "First %s, Second %s" firstNo secondNo
+
+    let sum = (int firstNo) + (int secondNo)
     printfn "The sum is %i" sum
     0
