6a6d8d3f34a9a36dc07719d1baad5eddbdf566cc
Roman Lazunin <rlazunin@gmail.com>
2026-03-22 19:49:11 +0100

    create explicit entry point and move code into it

--- fsharpspec_lab/Program.fs
+++ fsharpspec_lab/Program.fs
@@ -1,6 +1,17 @@
-﻿printfn "Hello from F#"
+﻿open System
 
-let numbers = [ 1 .. 10 ]
-let square x = x * x
-let squares = List.map square numbers
-printfn "N^2 = %A" squares
+// Define a function to construct a message to print
+let from whom =
+    sprintf "from %s" whom
+
+[<EntryPoint>]
+let main argv =
+    let message = from "F#" // Call the function
+    printfn "Hello world %s" message
+
+    let numbers = [ 1 .. 10 ]
+    let square x = x * x
+    let squares = List.map square numbers
+    printfn "N^2 = %A" squares
+
+    0 // return an integer exit code
--- ms-learn-fsharp-first-steps/MyFSharpApp/Program.fs
+++ ms-learn-fsharp-first-steps/MyFSharpApp/Program.fs
@@ -1,2 +1,17 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+// Define a function to construct a message to print
+let from whom =
+    sprintf "from %s" whom
+
+[<EntryPoint>]
+let main argv =
+    let message = from "F#" // Call the function
+    printfn "Hello world %s" message
+
+    let numbers = [ 1 .. 10 ]
+    let square x = x * x
+    let squares = List.map square numbers
+    printfn "N^2 = %A" squares
+
+    0 // return an integer exit code
