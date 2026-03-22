ec6d867eabd2dd198ea42c7c71e8f865adaef1a9
Roman Lazunin <rlazunin@gmail.com>
2026-03-22 22:50:44 +0100

    add for loops

--- ms-learn-fsharp-first-steps/Cards/Program.fs
+++ ms-learn-fsharp-first-steps/Cards/Program.fs
@@ -7,3 +7,15 @@ let cardDescription =
    elif cardNo = 13 then "King"
    else string cardNo
 printfn "%s" cardDescription
+
+let list = [1; 2; 3; 4; 5]
+for i in list do
+   printf "%d " i
+
+printfn ""
+for i = 1 to 10 do
+  printf "%i " i  // prints 1 2 3 4 5 6 7 8 9 10
+
+printfn ""
+for i = 10 downto 1 do
+  printf "%i " i  // prints 10 9 8 7 6 5 4 3 2 1
