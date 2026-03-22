04133b2ee57be920fcd477cf4748c68618e8c52b
Roman Lazunin <rlazunin@gmail.com>
2026-03-22 22:03:42 +0100

    create project Variables; add main code with explicit entry point

---
+++ ms-learn-fsharp-first-steps/Variables/Program.fs
@@ -0,0 +1,7 @@
+﻿[<EntryPoint>]
+let main argv =
+    printfn "Welcome to the calculator program"
+    // read input from the console and assign to `sum`
+    let sum = 0
+    printfn "The sum is %i" sum
+    0
