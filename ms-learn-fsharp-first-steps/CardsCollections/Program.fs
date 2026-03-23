905a680ba797f46ab09393446839546952cbe10e
Roman Lazunin <rlazunin@gmail.com>
2026-03-23 20:33:58 +0100

    add code examples and finish the course

---
+++ ms-learn-fsharp-first-steps/CardsCollections/Program.fs
@@ -0,0 +1,62 @@
+﻿let cards = [ 0 .. 5 ]
+let hand = []
+
+let drawCard (tuple: int list * int list) = 
+    let deck = fst tuple
+    let draw = snd tuple
+    let firstCard = deck.Head
+    printfn "%i" firstCard
+
+    let hand = 
+        draw
+        |> List.append [firstCard]
+
+    (deck.Tail, hand)
+
+let d, h = (cards, hand) |> drawCard |> drawCard
+
+printfn "Deck: %A Hand: %A" d h // Deck: [2; 3; 4; 5] Hand: [1; 0]
+
+let cards2 = [ 1 .. 5 ]
+List.iter(fun i -> printf "%i " i) cards2 // 1 2 3 4 5
+
+printfn ""
+for i in cards do printf "%i " i
+
+type Person = { FirstName: string; LastName: string  }
+let people = [
+   { FirstName="Albert"; LastName= "Einstein" }
+   { FirstName="Marie"; LastName="Curie" }
+]
+let nobelPrizeWinners = List.map (fun person -> person.FirstName + person.LastName) people 
+printfn "%A" nobelPrizeWinners // ["Albert Einstein"; "Marie Curie"]
+
+let filteredList = List.filter(fun i-> i % 2 = 0) cards2
+List.iter(fun i -> printfn "item %i" i) filteredList // item 2 item 4
+
+let list = [2; 1; 5; 3]
+let sortedList = List.sort list // 1 2 3 5
+
+let fruits = ["Banana"; "Apple"; "Pineapple"]
+let sortedFruits = List.sortBy (fun (fruit : string) -> fruit.Length) fruits // Apple, Banana, Pineapple
+
+// assume a type like so
+type MagicCreature = { Name : string; Level: int; Attack: int }
+let creatures = [
+   { Name="Dragon"; Level=2; Attack=20 }
+   { Name="Orc"; Level=1; Attack=5 }
+   { Name="Demon"; Level=2; Attack=10 } 
+]
+
+// comparison function, -1 = less than, 1 = larger than, 0 = equal
+let compareCreatures c1 c2 =
+     if c1.Level < c2.Level then -1
+     else if c1.Level > c2.Level then 1
+     else if c1.Attack < c2.Attack then -1
+     else if c1.Attack > c2.Attack then 1
+     else 0
+
+let sorted = List.sortWith compareCreatures creatures // { Name="Orc"; Level=1; Attack=5 }, { Name="Demon"; Level=2; Attack=10 }, { Name="Dragon"; Level=2; Attack=20 }
+
+let list2 = [1; 2; 3; 4]
+let found = List.find( fun x -> x % 2 = 0) list2 // 2 - Only the first element that matches the condition is returned.
---
+++ ms-learn-fsharp-first-steps/Collections/Program.fs
@@ -0,0 +1,26 @@
+﻿let logEntryMonday = "first entry"
+let logEntryTuesDay = "second entry"
+let logEntryWednesday = "third entry"
+
+let logEntriesWeek = ["first entry"; "second entry"; "third entry"]
+
+let cards2 = [
+  "Ace"
+  "King"
+  "Queen"
+]
+
+let numbers = [ 1 .. 5 ]
+
+let cards = ["Ace"; "King"; "Queen"]
+
+let newList = "Jack" :: cards // "Jack", "Ace", "King", "Queen" 
+let otherCardList = ["Jack"; "10"]
+let fullList = cards @ otherCardList // "Ace", "King", "Queen", "Jack", "10"
+
+let otherCardList2 = ["10"; "9"]
+let fullList2 = cards |> List.append ["Jack"] // "Jack", "Ace", "King", "Queen"
+let fullList3 = cards |> List.append otherCardList2 // "10", "9", "Ace", "King", "Queen"
+
+let list = [1; 2; 3; 4]
+let itemOne = list.Item 1 // 2
