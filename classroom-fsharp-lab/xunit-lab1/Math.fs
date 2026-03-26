056286f2e2a322a97b5ee06afe6b603c1bbed48b
Roman Lazunin <romanlazunin@users.noreply.github.com>
2026-03-26 10:46:32 +0000

    add Math module; add more simple tests

---
+++ classroom-fsharp-lab/xunit-lab1/Math.fs
@@ -0,0 +1,3 @@
+module Math
+
+let add a b = a + b
--- classroom-fsharp-lab/xunit-lab1/Tests.fs
+++ classroom-fsharp-lab/xunit-lab1/Tests.fs
@@ -2,7 +2,22 @@
 
 open System
 open Xunit
+open Math
 
 [<Fact>]
 let ``My test`` () =
     Assert.True(true)
+
+[<Fact>]
+let ``1 + 1 = 2`` () =
+    Assert.Equal(2, 1 + 1)
+
+[<Fact>]
+let ``basic arithmetic works`` () =
+    Assert.Equal(4, 2 + 2)
+    Assert.Equal(0, 2 - 2)
+    Assert.True(3 > 1)
+
+[<Fact>]
+let ``add 2 3 = 5`` () =
+    Assert.Equal(5, add 2 3)
