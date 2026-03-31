6b2c38dc96fca9cb3220d4b33f84ca317c56b542
Roman Lazunin <romanlazunin@users.noreply.github.com>
2026-03-31 13:41:19 +0000

    add code examples for lesson Functions

--- linkedin-learn-intro-fsharp/Intro2FSharp/Program.fs
+++ linkedin-learn-intro-fsharp/Intro2FSharp/Program.fs
@@ -1,5 +1,12 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-// printfn "Hello from F#"
+﻿open System
+
+// Functions & expressions
+let increaseByOne x = x + 1
+let areBothTrue x y = x && y
+let addValues (x: string) (y: string) : string = x + y
+let increment = increaseByOne
+let applyAndDouble (fn:int -> int) (x:int) = fn x * 2
+let rec fib n = if n < 2 then 1 else fib(n - 1) + fib(n - 2)
 
 // Binding values
 let welcome: string = "Welcome to "
@@ -10,24 +17,41 @@ courseTitle <- "Intro 2 FSharp!"
 
 
 // Operations
-let message = welcome + courseTitle
+let message = addValues welcome courseTitle
 
 // Immutability
 let capMessage = message.ToUpper()
 
 let energyLevel: int = 10
-let turnItToEleven = energyLevel + 1
+// let turnItToEleven = increment energyLevel
+let turnItToEleven = applyAndDouble increaseByOne 10
 
 // Booleans
 let awake: bool = true
 let readyToGo = turnItToEleven >= 11
-let start = awake && readyToGo
+// let start = areBothTrue awake readyToGo
+let startPartial = areBothTrue awake
+let start = startPartial readyToGo
+let stop = startPartial false
+
+// Console input
+let getUsername() : string = 
+    printfn "Enter your name:"
+    Console.ReadLine()
 
 // Printing  to console
-printfn "%s" message
-printfn $"{message}"
-printfn $"\nEnergy level is at {energyLevel} but I need it to go to {turnItToEleven}"
-printfn $"Are you ready? {start}"
+let printWelcome() =
+    printfn $"\n{message}"
+
+let debug (name:string) =
+    printfn $"\nEnergy level is at {energyLevel} but I need it to go to {turnItToEleven}"
+    printfn $"Are you ready {name}? {start}"
+
+let name = getUsername()
+printWelcome()
+debug name
+
+Console.ReadKey() |> ignore
 
 (*/// Explicit entry point
 [<EntryPoint>]
